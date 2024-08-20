ad04bbb2dc7c30375c186bc44a7bffefac1e02ba
Marco Antonio Rodrigues Gomes de Souza <94053824+Marco-Antonio-Rodrigues@users.noreply.github.com>
2024-08-20 08:02:57 -0300

    🎉: commit init

---
+++ Codigos/pda_python/main.py
@@ -0,0 +1,11 @@
+# .\.venv\Scripts\python.exe -m pip install readchar #import threading #import readchar
+from Test import *
+
+if __name__ == "__main__": 
+    Test.teste_y_x('00001111')
+    #Test.reverso('1001001001')
+    #Test.calc('a*a+(a+a)')
+    #Test.enquanto('eqt(a){eqt(a){}}') # mais formas?
+
+
+
